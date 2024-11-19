3c6fbac2c0909f5b1199cec41dd91a55cb223706
Francis Hilaire <Prometee@users.noreply.github.com>
2024-11-19 20:01:32 +0100

    Constraint payload to require success and cancel urls

---
+++ src/Validator/Constraints/CheckoutSessionCreatePayloadRequirement.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FluxSE\SyliusStripePlugin\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+final class CheckoutSessionCreatePayloadRequirement extends Constraint
+{
+    public string $noSuccessUrlFound = 'flux_se_sylius_stripe_plugin.stripe_checkout.success_url.not_found';
+    public string $noCancelUrlFound = 'flux_se_sylius_stripe_plugin.stripe_checkout.cancel_url.not_found';
+
+    public function validatedBy(): string
+    {
+        return 'flux_se_sylius_stripe_checkout_session_create_payload_requirement';
+    }
+}
---
+++ src/Validator/Constraints/CheckoutSessionCreatePayloadRequirementValidator.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FluxSE\SyliusStripePlugin\Validator\Constraints;
+
+use Sylius\Bundle\ApiBundle\Command\Payment\AddPaymentRequest;
+use Sylius\Bundle\PaymentBundle\Provider\GatewayFactoryNameProviderInterface;
+use Sylius\Component\Core\Model\PaymentMethodInterface;
+use Sylius\Component\Core\Repository\PaymentMethodRepositoryInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Webmozart\Assert\Assert;
+
+final class CheckoutSessionCreatePayloadRequirementValidator extends ConstraintValidator
+{
+    /**
+     * @param string[] $supportedFactoryNames
+     * @param string[] $supportedActions
+     */
+    public function __construct(
+        private PaymentMethodRepositoryInterface $paymentMethodRepository,
+        private GatewayFactoryNameProviderInterface $gatewayFactoryNameProvider,
+        private array $supportedFactoryNames,
+        private array $supportedActions,
+    ) {
+    }
+
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        /** @var CheckoutSessionCreatePayloadRequirement $constraint */
+        Assert::isInstanceOf($constraint, CheckoutSessionCreatePayloadRequirement::class);
+
+        /** @var AddPaymentRequest $addPaymentRequest */
+        $addPaymentRequest = $this->context->getObject();
+        Assert::isInstanceOf($addPaymentRequest, AddPaymentRequest::class);
+
+        /** @var PaymentMethodInterface|null $paymentMethod */
+        $paymentMethod = $this->paymentMethodRepository->findOneBy([
+            'code' => $addPaymentRequest->paymentMethodCode
+        ]);
+
+        if (null === $paymentMethod) {
+            return;
+        }
+
+        if (false === in_array(
+            $this->gatewayFactoryNameProvider->provide($paymentMethod),
+            $this->supportedFactoryNames,
+            true
+        )) {
+            return;
+        }
+
+        if (false === in_array(
+            $addPaymentRequest->action,
+            $this->supportedActions,
+            true
+        )) {
+            return;
+        }
+
+        if (false === isset($value['success_url'])) {
+            $this->context->addViolation($constraint->noSuccessUrlFound);
+        }
+
+        if (false === isset($value['cancel_url'])) {
+            $this->context->addViolation($constraint->noCancelUrlFound);
+        }
+    }
+}
