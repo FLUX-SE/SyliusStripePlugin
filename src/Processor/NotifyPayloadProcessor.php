9a0c736d198f38c45d570d5f23af150b3db412ee
Francis Hilaire <Prometee@users.noreply.github.com>
2025-03-24 17:36:07 +0100

    Do not add event to the payload when the factory name is not one of the supported ones

--- src/Processor/NotifyPayloadProcessor.php
+++ src/Processor/NotifyPayloadProcessor.php
@@ -10,18 +10,25 @@ use Symfony\Component\HttpFoundation\Request;
 
 final class NotifyPayloadProcessor implements NotifyPayloadProcessorInterface
 {
+    /**
+     * @param string[] $supportedFactories
+     */
     public function __construct(
         private NotifyPayloadProcessorInterface $decoratedNotifyPayloadProcessor,
+        private array $supportedFactories,
     ) {
     }
 
     public function process(PaymentRequestInterface $paymentRequest, Request $request): void
     {
-        $data = $request->toArray();
+        $factoryName = $paymentRequest->getMethod()->getGatewayConfig()?->getFactoryName() ?? '';
+        if (in_array($factoryName, $this->supportedFactories, true)) {
+            $data = $request->toArray();
 
-        $paymentRequest->setPayload([
-            'event' => $data,
-        ]);
+            $paymentRequest->setPayload([
+                'event' => $data,
+            ]);
+        }
 
         $this->decoratedNotifyPayloadProcessor->process($paymentRequest, $request);
     }
